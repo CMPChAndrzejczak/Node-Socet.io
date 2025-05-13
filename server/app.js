841d0b15873e54aa1b0316f4405271d0a533aae2
CMPChAndrzejczak <chevyprogrammer992@gmail.com>
2025-05-13 13:30:09 -0400

    Serves index.html now!

--- server/app.js
+++ server/app.js
@@ -9,6 +9,11 @@ const io = new Server(server, {
   cors: { origin: "*" },
 });
 
+app.use(express.static(path.join(__dirname, "client")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 io.on("connection", (socket) => {
   console.log("User connected", socket.id);
 
@@ -17,6 +22,7 @@ io.on("connection", (socket) => {
   });
 });
 const PORT = process.env.PORT || 3001;
+
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
